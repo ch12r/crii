7a12f5df92347c90932677ea7a7ea4841167233a
ch12r <info@christian-renner.eu>
2014-03-22 15:09:15 +0100

    inits Controller

---
+++ web/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ch12r\crii\web;
+
+/**
+ * Controller.php
+ *
+ * @author Christian Renner <info@christian-renner.eu>
+ */ 
+class Controller extends \yii\web\Controller
+{
+
+}
+ 
